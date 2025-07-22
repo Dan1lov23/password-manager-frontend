178f62075ae7a94e90021270cbb342fd693fbcbd
kitik <svy1dan@gmail.com>
2025-07-22 13:18:17 +0300

    types and interfaces

---
+++ src/types and interfaces/Interfaces.tsx
@@ -0,0 +1,11 @@
+export interface Password {
+    serviceName: string;
+    password: string;
+    passwordId: number;
+}
+
+export interface User {
+    username:string,
+    password:string,
+    userRole:string,
+}
