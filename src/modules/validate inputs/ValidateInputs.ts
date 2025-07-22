42d6a1d5d5307c66e64e3c0ace5bd8f01e1864b7
kitik <svy1dan@gmail.com>
2025-07-22 13:17:20 +0300

    validate inputs module

---
+++ src/modules/validate inputs/ValidateInputs.ts
@@ -0,0 +1,89 @@
+const validateInputs = (username:string, password:string, repeatPassword:string) => {
+
+    if (password !== repeatPassword) {
+        return "Пароли не совпадают"
+    }
+
+    const uppercaseLetters: string[] = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+
+    const lowercaseLetters: string[] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+
+    const numbersArray:string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    const specialCharacters: string[] = [
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',
+        '[', ']', '{', '}', '|', ':', ';', '"', '<', '>', ',', '.', '?', '/'
+    ];
+
+    const splitPassword = password.split("")
+
+    if (username.length < 5) {
+        return "Длинна username должна быть больше 5 символов"
+    }
+
+    if (password.length < 5) {
+        return "Длинна пароля должна быть больше 5 символов"
+    }
+
+    const uppercasePasswordValidator = () => {
+        for (let a = 0; a < splitPassword.length; a++) {
+            if (uppercaseLetters.includes(splitPassword[a])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (!uppercasePasswordValidator()) {
+        return "Пароль должен содержать хотя бы одну заглавную букву"
+    }
+
+    const lowercasePasswordValidator = () => {
+        for (let a = 0; a < splitPassword.length; a++) {
+            if (lowercaseLetters.includes(splitPassword[a])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (!lowercasePasswordValidator()) {
+        return "Пароль должен содержать хотя бы одну маленькую букву"
+    }
+
+    const numbersValidatePassword = () => {
+        for (let a = 0; a < splitPassword.length; a++) {
+            if (numbersArray.includes(splitPassword[a])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (!numbersValidatePassword()) {
+        return "Пароль должен содержать хотя бы одну цифру"
+    }
+
+    const specialCharactersValidator = () => {
+        for (let a = 0; a < splitPassword.length; a++) {
+            if (specialCharacters.includes(splitPassword[a])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (!specialCharactersValidator()) {
+        return "Пароль должен содержать хотя бы один спец символ"
+    }
+
+    return true;
+}
+
+export default validateInputs;
